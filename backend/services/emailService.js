bd4fe49ead9da413e7b7cdb437464dcd9caaadc5
KavinduLakmal2000 <kavindulakmal2000@gmail.com>
2026-08-21 02:11:02 +0530

    Use Gmail SMTP port 587

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -37,8 +37,8 @@ function createTransporter() {
 
   transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     family: 4,
 
     auth: {
